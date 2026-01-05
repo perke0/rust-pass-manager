30051bc072cc98de293e7de9778c114bec31cf0e
perke0 <93860129+perke0@users.noreply.github.com>
2026-01-05 17:03:53 +0100

    crypt dn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#![allow(dead_code, unused)]
+mod io {
+    fn get_input() -> String {
+        use std::io;
+        println!("Type password here:");
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input from user");
+
+        input
+    }
+}
+mod crypt {
+    use hex_literal::hex;
+    use sha2::{Digest, Sha512};
+
+    pub fn hashing() -> bool {
+        let mut hasher = Sha512::new();
+        hasher.update(b"hello world");
+        let result = hasher.finalize();
+
+        result[..]
+            == hex!(
+                "309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f
+    989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f"
+            )[..]
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    use crypt;
+    let s: bool = crypt::hashing();
+    println!("{s}");
 }
